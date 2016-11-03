53820c1df223bc675f030f12da129b4f481da3fd
Graeme Bell <bell.graeme@gmail.com>
2016-11-03 07:09:17 +0000

    homework

--- HomeworkDone.rb
+++ HomeworkDone.rb
@@ -3,69 +3,67 @@
 # # ### A. Given the following data structure:
 
 # # ```
-#  lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'Princes Street']
+lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'Princes Street']
 # # # ```
 
 # # # 1. Work out how many stops there are in the lines array
-#  stops = lines.length
-#  puts stops
+ stops = lines.length
+ puts stops
 #  # 2. Return 'Edinburgh Park' from the array
-#  puts lines[1]
+ puts lines[1]
 # # # 3. How many ways can we return 'Princes Street' from the array?
-# puts lines[5]
+puts lines[5]
 # # # 4. Work out the index position of 'Haymarket' 
-# puts lines.index("Haymarket")
+puts lines.index("Haymarket")
 
 # # # 5. Add 'Airport' to the start of the array 
-# lines.insert(0,'Airport')
-# puts lines
+ lines.insert(0,'Airport')
+ puts lines
 # # # 6. Add 'York Place' to the end of the array
-# lines.insert(-1, "York Place")
-#  puts lines
+ lines.insert(-1, "York Place")
+ puts lines
 # # # 7. Remove 'Edinburgh Park' from the array using it's name 
-# lines.delete("Edinburgh Park")
+ lines.delete("Edinburgh Park")
 # # # 8. Delete 'Edinburgh Park' from the array by index
-# lines.delete_at(2)
+ lines.delete_at(2)
 # # # 9. Reverse the positions of the stops in the array
-#  puts lines.reverse
+  puts lines.reverse
 # # # 10. Print out all of the stops using a for loop and a while loop
-# def prints_stops(array)
-#   for x in array
-    
-#     puts x
+ def prints_stops(array)
+   for x in array
+     puts x
+   end
+ end
 
-#   end
-# end
+ def prints_stops_while(array)
+   counter = 0
+   while (counter < array.length)
+     puts array[counter]
+     counter += 1
+   end
+ end
 
-# def prints_stops_while(array)
-#   counter = 0
-#   while (counter < array.length)
-#     puts array[counter]
-#     counter += 1
-#   end
-# end
-
-#  prints_stops(lines)
-# prints_stops_while(lines)
+prints_stops(lines)
+prints_stops_while(lines)
 
 # # ### B. Given the following data structure:
 
 # # ```
-# my_hash = {0 => "Zero", 1 => "One", :two => "Two", "two" => 2}
+ my_hash = {0 => "Zero", 1 => "One", :two => "Two", "two" => 2}
 # # ```
 
 # # 1. How would you return the string `"One"`?
-#   puts my_hash[1]
+   puts my_hash[1]
 # # 2. How would you return the string `"Two"`?
-# puts my_hash[:two]
+ puts my_hash[:two]
 # # 3. How would you return the number `2`?
-# puts my_hash["two"]
+ puts my_hash["two"]
 # # 4. How would you add `{3 => "Three"}` to the hash? 
-# my_hash.merge!( {3 => "Three"})
-# print my_hash 
+ my_hash.merge!( {3 => "Three"})
+ print my_hash 
 # # 5. How would you add `{:four => 4}` to the hash? 
-# my_hash.merge!( {:four => 4})
-# print my_hash
+ my_hash.merge!( {:four => 4})
+ print my_hash
 # # ### C. Given the following data structure:
 
 # ```
